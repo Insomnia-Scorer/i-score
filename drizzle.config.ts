061024c515349e85ef1ce85a59ca28b4cfee11fc
Insomnia-Scorer <insomniascorer@gmail.com>
2026-02-20 15:53:00 +0000

    Setup better auth

---
+++ drizzle.config.ts
@@ -0,0 +1,9 @@
+// drizzle.config.ts
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+    dialect: "sqlite",
+    schema: "./src/db/schema.ts",
+    out: "./drizzle",
+});
+
