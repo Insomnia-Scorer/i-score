29cb3ec032d2b063cac384a3209c24c84452f43f
Insomnia-Scorer <insomniascorer@gmail.com>
2026-02-21 17:56:36 +0900

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,21 +1,6 @@
 // open-next.config.ts
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-const config = {
-  default: {
-    runtime: "edge",
-    // 💡 これが肝！ 機能を分割して 1 ファイルあたりのサイズを劇的に減らします
-    splitting: true, 
-    minify: true,
-  },
-  // Middleware を別関数として抽出し、軽量化
-  middleware: {
-    external: ["better-auth/cookies"],
-  }
-};
-
-export default config;
-
 export default defineCloudflareConfig({
 	// Uncomment to enable R2 cache,
 	// It should be imported as:
