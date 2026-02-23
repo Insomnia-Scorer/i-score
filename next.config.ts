b7aca95d1685d8665ff4aa023c243e50614abfe9
Insomnia-Scorer <insomniascorer@gmail.com>
2026-02-23 13:55:34 +0000

    Update files

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // これが Workers での外部パッケージ解決の鍵です
   serverExternalPackages: ["better-auth", "drizzle-orm", "@better-auth/cloudflare-d1"],
-
   // 💡 Turbopack を一旦完全にオフにし、安定した Webpack ビルドを強制します
   experimental: {},
-
+  output: 'standalone',
   // Cloudflare の画像最適化機能を使わずに、標準の画像として扱います
   images: {
     unoptimized: true,
