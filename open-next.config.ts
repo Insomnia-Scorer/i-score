91e1b26f69bdefcc7b336e68041e7b37f66bc660
Insomnia-Scorer <insomniascorer@gmail.com>
2026-02-23 06:11:26 +0000

    Delete open-next.config.ts

--- open-next.config.ts
+++
@@ -1,7 +0,0 @@
-// open-next.config.ts
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-export default defineCloudflareConfig({
-  // 💡 今のバージョンでは、何も指定しないのが最も安全です。
-  // 💡 必要な最適化は OpenNext が自動で行います。
-});
